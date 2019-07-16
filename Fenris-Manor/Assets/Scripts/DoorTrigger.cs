6f17f78a66978f8e451bb51d4e3a833ecbb1ce96
Derrick Tubbs <derrick.j.tubbs@gmail.com>
2019-07-16 19:47:31 -0400

    I legit don't know what all this is I waited too long to commit

--- Fenris-Manor/Assets/Scripts/DoorTrigger.cs
+++ Fenris-Manor/Assets/Scripts/DoorTrigger.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class DoorTrigger : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	/* public void OnTriggerEnter2D(Collider2D collision)
+    {
+        //print(collision.gameObject.tag);
+        if (collision.gameObject.tag != "Player")
+            return;
+ 
+        Messenger.Broadcast("roomChanged",roomName);
+        camera1.gameObject.SetActive(false);
+        camera2.gameObject.SetActive(true);
+       
+        collision.gameObject.transform.position = whereToSpawn.transform.position;
+    } */
 }
