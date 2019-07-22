c9ca888ab444c86764867ec39c8f618d2081cc52
Derrick Tubbs <derrick.j.tubbs@gmail.com>
2019-07-22 01:29:36 -0400

    Initial Commit

---
+++ Fenris-Manor/Assets/Scripts/PlayerScripts/StairController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StairController : MonoBehaviour
+{
+    public enum STAIR_FACING {
+        Left,
+        Right
+    }
+
+    public STAIR_FACING stairDirection;
+    public GameObject upTrigger;
+    public GameObject downTrigger;
+    public int numSteps;
+
+    protected void playerClimbStairs() {
+        
+    }
+
+
+}
