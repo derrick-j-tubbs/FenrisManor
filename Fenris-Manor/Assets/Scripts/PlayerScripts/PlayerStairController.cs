9fa375ab06b00f32c77500c77c54aa3d52e2e66c
Derrick Tubbs <derrick.j.tubbs@gmail.com>
2019-07-22 23:26:51 -0400

    Initial Commit

---
+++ Fenris-Manor/Assets/Scripts/PlayerScripts/PlayerStairController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStairController : MonoBehaviour
+{
+    private enum STAIR_STATE {
+        on_stair,
+        near_stair,
+        off_stair
+    }
+    
+    // Transform location for actor to start climbing from
+    // public Transform startClimb;
+    // public STAIR_STATE playerStairState;
+
+   public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag != "Stairs")
+            return;
+
+    }
+}
