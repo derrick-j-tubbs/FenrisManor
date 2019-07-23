140582fd30a48e59fa9830c2e85aa33362c2bcda
Derrick Tubbs <derrick.j.tubbs@gmail.com>
2019-07-22 23:26:04 -0400

    Moved StairController out of Player

---
+++ Fenris-Manor/Assets/Scripts/StairController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StairController : MonoBehaviour
+{
+    public enum STAIR_FACING {
+        Left,
+        Right
+    };
+
+    public enum TRIGGER_TYPE {
+        Up,
+        Down
+    };
+
+    private int _numSteps = 0;
+    private float _stairSize = 0.5f;
+
+    protected float stairHeight = 0.5f;
+    protected float stairLength = 0.5f;
+
+    public STAIR_FACING stairDirection;
+    public GameObject endOfStairs;
+    public TRIGGER_TYPE prepDirection;
+    
+}
