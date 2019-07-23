36b68348b9c99e0403bd28f0365e2d27e911d4cf
Derrick Tubbs <derrick.j.tubbs@gmail.com>
2019-07-22 23:29:54 -0400

    Explained purpose of class

--- Fenris-Manor/Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Fenris-Manor/Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    /*
+     *  This will be the overarching PlayerController class, this will hold values used by all the other classes and be the only one with values expsed in the 
+     *  editor. Determining how to organize these so that they are meaningful and grouping them so that they can be expanded and collapsed when in use will be 
+     *  important. Initially will hold things like 'Player Max Speed' and 'Player Jump Force" but will also contains state_based things for use with both 
+     *  animation and logic. This could be enumerators like Stair_State or values such as whip level / sub weapon level.
+     */
 }
