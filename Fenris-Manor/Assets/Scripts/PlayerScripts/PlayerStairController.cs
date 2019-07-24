6b6af70852d8dc0f1ce2999c06eaf865e5c5136c
Derrick Tubbs <derrick.j.tubbs@gmail.com>
2019-07-23 22:11:28 -0400

    Initial Implementation See Todo for remaining tasks

--- Fenris-Manor/Assets/Scripts/PlayerScripts/PlayerStairController.cs
+++ Fenris-Manor/Assets/Scripts/PlayerScripts/PlayerStairController.cs
@@ -4,20 +4,121 @@ using UnityEngine;
 
 public class PlayerStairController : MonoBehaviour
 {
-    private enum STAIR_STATE {
-        on_stair,
-        near_stair,
-        off_stair
-    }
-    
-    // Transform location for actor to start climbing from
-    // public Transform startClimb;
-    // public STAIR_STATE playerStairState;
+    public float transitionSpeed = 3f;
+    private float fraction = 0;
 
-   public void OnTriggerEnter2D(Collider2D collision)
+    public int stairClimbFrameDuration = 6;
+
+    private Animator animator;
+    private PlayerController playerController;
+    private GameObject player;
+    private PlayerController.STAIR_STATE playerStairState = PlayerController.STAIR_STATE.off_stair;
+    private StairController.STAIR_DIRECTION stairDirection;
+
+    private void Awake() {
+        player = GameObject.Find("Player");
+        playerController = player.GetComponent<PlayerController>();
+        animator = playerController.PlayerAnimator;
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
     {
+        PlayerPlatformerController platformerController = player.GetComponent<PlayerPlatformerController>();
+        
+        GameObject stairs = collision.gameObject;
+        StairController stairController = stairs.GetComponent<StairController>();
+
+        GameObject closestEndStep;
+
         if (collision.gameObject.tag != "Stairs")
             return;
-        Debug.Log("You did it!");
+        stairDirection = stairController.getStairDirection();
+        float climb = Input.GetAxis("Climb");
+        if (climb > 0.1f)
+        {
+            platformerController.enabled = false;
+            //Debug.Log("Control taken from platformer controller");
+            // move player to base of stairs if grounded [may require animation]
+            if (platformerController.PlayerGrounded())
+            {
+                closestEndStep = FindClosestEnd(stairController, player);
+                //Debug.Log("Closest End: " + closestEndStep.name);
+                StartCoroutine(MovePlayerToStairs(player, closestEndStep));
+                playerStairState = PlayerController.STAIR_STATE.on_stair;
+            }
+        }
+    }
+
+    private void Update() {
+        if (playerStairState == PlayerController.STAIR_STATE.on_stair) {
+            if (Input.GetKey("d")) {
+                Debug.Log("Moving Right on Stairs");
+                StartCoroutine(MoveRightOnStairs(player));
+            } else if (Input.GetKey("a")) {
+                Debug.Log("Moving Left on Stairs");
+                StartCoroutine(MoveLeftOnStairs(player));
+            }
+        }
+    }
+
+    private GameObject FindClosestEnd(StairController stairs, GameObject player) {
+        float leftDifference = Mathf.Abs(stairs.leftEndStep.transform.position.x - player.transform.position.x);
+        float rightDifference = Mathf.Abs(stairs.rightEndStep.transform.position.x - player.transform.position.x);
+        if (leftDifference < rightDifference)
+            return stairs.leftEndStep;
+        return stairs.rightEndStep;
+    }
+
+    IEnumerator MovePlayerToStairs(GameObject player, GameObject closestEndStep) {
+        Vector2 posPlayer = new Vector2(player.transform.position.x, closestEndStep.transform.position.y + 1);
+        Vector2 posStep = new Vector2(closestEndStep.transform.position.x, closestEndStep.transform.position.y + 1);
+
+        this.enabled = false;
+        while (fraction < 1) {
+            fraction += Time.deltaTime * transitionSpeed;
+            if (fraction > 1)
+                fraction = 1;
+            player.transform.position = Vector2.Lerp(posPlayer, posStep, fraction);
+            yield return new WaitForEndOfFrame();
+        }
+        this.enabled = true;
+    }
+
+    IEnumerator MoveLeftOnStairs(GameObject player) {
+        float animationDelay = 6f / 60f;
+        Debug.Log("Animation Delay: " + animationDelay);
+        Vector2 moveTo = Vector2.zero;
+        if (stairDirection == StairController.STAIR_DIRECTION.Up) {
+            float newX = player.transform.position.x - 0.5f;
+            float newY = player.transform.position.y - 0.5f;
+            moveTo = new Vector2(newX, newY);
+            animator.Play("PlayerDownStairs");
+        } else if (stairDirection == StairController.STAIR_DIRECTION.Down) {
+            float newX = player.transform.position.x + 0.5f;
+            float newY = player.transform.position.y + 0.5f;
+            moveTo = new Vector2(newX, newY);
+            animator.Play("PlayerUpStairs");
+        } 
+        player.transform.position = Vector2.Lerp(player.transform.position, moveTo, 1);
+        yield return new WaitForSeconds(1f);
+    }
+
+    IEnumerator MoveRightOnStairs(GameObject player) {
+        float animationDelay = 6f / 60f;
+        Debug.Log("Animation Delay: " + animationDelay);
+        Vector2 moveTo = Vector2.zero;
+        if (stairDirection == StairController.STAIR_DIRECTION.Up) {
+            float newX = player.transform.position.x + 0.5f;
+            float newY = player.transform.position.y + 0.5f;
+            moveTo = new Vector2(newX, newY);
+            animator.Play("PlayerUpStairs");
+        } else if (stairDirection == StairController.STAIR_DIRECTION.Down) {
+            float newX = player.transform.position.x - 0.5f;
+            float newY = player.transform.position.y - 0.5f;
+            moveTo = new Vector2(newX, newY);
+            animator.Play("PlayerDownStairs");
+        } 
+        player.transform.position = Vector2.Lerp(player.transform.position, moveTo, 1);
+        yield return new WaitForSeconds(animationDelay);
     }
 }
