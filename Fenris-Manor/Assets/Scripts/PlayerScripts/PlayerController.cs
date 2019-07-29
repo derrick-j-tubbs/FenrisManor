0e5c1fbd7e71781c10d5b18a88b7d14b9598c570
Derrick Tubbs <derrick.j.tubbs@gmail.com>
2019-07-28 23:22:10 -0400

    Added comments to Player class

--- Fenris-Manor/Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Fenris-Manor/Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -10,6 +10,10 @@ public class PlayerController : MonoBehaviour
     *  editor. Determining how to organize these so that they are meaningful and grouping them so that they can be expanded and collapsed when in use will be 
     *  important. Initially will hold things like 'Player Max Speed' and 'Player Jump Force" but will also contains state_based things for use with both 
     *  animation and logic. This could be enumerators like Stair_State or values such as whip level / sub weapon level.
+    *  
+    *  There is a consideration for the possibility of renaming this class simply to the Player class, as it will contain the values for all the scripts and
+    *  states of the player throughout the course of gameplay and provides no controls. This change is minor and only in name and not super relevant in the
+    *  long run, mostly this is just for the purpose of maintaining good object oriented practices, which isn't super important.
     */
 
     public GameObject Player;
@@ -25,8 +29,14 @@ public class PlayerController : MonoBehaviour
         right
     }
 
+    // create values for all the previous enums
     protected STAIR_STATE playerStairState;
     protected PLAYER_FACING playerFacing;
+
+    // create boolean values to allow other classes to change thes tate of the player
+    // considerations are being made as to whether to make this an enum like the two above
+    // this would give us more control, howevver players can exist in two states at once for many of the states on this list
+    // likely it is better for each of them to remain as individual boolean variables
     protected bool isClimbing;
 
     void Awake(){
@@ -37,6 +47,8 @@ public class PlayerController : MonoBehaviour
         isClimbing = false;
     }
 
+
+    // create public getters and setters for anything that another class will need access to
     public STAIR_STATE getPlayerStairState(){
         return playerStairState;
     }
